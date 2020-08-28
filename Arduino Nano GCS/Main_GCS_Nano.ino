6ba82cc750673981ca81eaf86b2bb526856ae118
Tharindu Suraj Chathuranga <suraj.tharindu@gmail.com>
2020-08-29 00:13:33 +0530

    Formatting added

--- Arduino Nano GCS/Main_GCS_Nano.ino
+++ Arduino Nano GCS/Main_GCS_Nano.ino
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>                   //Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";    // NRF24L01 Address
 char textin[32] = "";
