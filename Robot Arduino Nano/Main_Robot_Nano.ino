806d9cff173a562b60d6eb590c1f7eaef2ae7dc3
Tharindu Suraj Chathuranga <suraj.tharindu@gmail.com>
2020-08-31 12:33:46 +0530

    I2C address updated

--- Robot Arduino Nano/Main_Robot_Nano.ino
+++ Robot Arduino Nano/Main_Robot_Nano.ino
@@ -1,52 +1,41 @@
-/* This code measures the distance from 8 sonar sensors and send the measurement 
- *  values to STM32.
- *  
- * Following is the sensor arrangement (top view) and measurement values are stored in the  
- * array according to that order. 
- * 
- *            1      2
- *        ---------------
- *       |      Front    |
- *     8 |               | 3
- *       |               | 
- *       |               | 
- *     7 |               | 4     
- *       |      Back     |
- *        ---------------
- *            6      5 
- */          
-
 
 #include <NewPing.h>
 #include <Wire.h>
+
 #define SONAR_NUM     8 // Number of sensors.
 #define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
 #define PING_INTERVAL 33 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
 
-#define MX_OBS_DIS 100 
-int x;
+#define obs_distance 30  //Threshold to detect obstacles
+
+//Represents obstacle status in front,left,rear and right respectively.
+//0 for obstacle free. 1 for obstacle detected.
+int obstacle_array[4] = {0,0,0,0};    
+
 unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
 unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
 uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
+unsigned int cm_filtered[SONAR_NUM];
 
+// Each sensor's trigger pin, echo pin, and max distance to ping.
 NewPing sonar[SONAR_NUM] = {     // Sensor object array.
-    NewPing(A0, A1, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
-    NewPing(A2, A3, MAX_DISTANCE),
-    NewPing(2, 3, MAX_DISTANCE),
-    NewPing(4, 5, MAX_DISTANCE),
-    NewPing(6, 7, MAX_DISTANCE),
-    NewPing(8, 9, MAX_DISTANCE),
-    NewPing(10, 11, MAX_DISTANCE),
-    NewPing(12, 13, MAX_DISTANCE),
+  NewPing(A0, A1, MAX_DISTANCE), //
+  NewPing(A2, A3, MAX_DISTANCE), //Front two sensors
+  NewPing(2, 3, MAX_DISTANCE),
+  NewPing(4, 5, MAX_DISTANCE),   //Left two sensors 
+  NewPing(6, 7, MAX_DISTANCE),
+  NewPing(8, 9, MAX_DISTANCE),    //Rear two sensors
+  NewPing(10, 11, MAX_DISTANCE),
+  NewPing(12, 13, MAX_DISTANCE),  // Right two sensors
 
 };
 
 void setup() {
-  Serial.begin(115200);
+
   pingTimer[0] = millis() + 75;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
   for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
     pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
-    
+  
   Wire.begin(); 
 }
 
@@ -61,10 +50,13 @@ void loop() {
       sonar[currentSensor].ping_timer(echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
     }
   }
-  // Other code that *DOESN'T* analyze ping results can go here.
-  Wire.beginTransmission(9); // transmit to device #9
-  Wire.write(x);              // sends x 
-  Wire.endTransmission(); 
+  // write obstacle data to I2C 
+  Wire.beginTransmission(4); // transmit to device #4
+  for (int i=0; i<5 ; i++){
+    Wire.write(obstacle_array[i]); 
+  }
+  Wire.endTransmission();    // stop transmitting
+
 }
 
 void echoCheck() { // If ping received, set the sensor distance to array.
@@ -73,28 +65,18 @@ void echoCheck() { // If ping received, set the sensor distance to array.
 }
 
 void oneSensorCycle() { // Sensor ping cycle complete, do something with the results.
-  // The following code would be replaced with your code that does something with the ping results.
-  for (uint8_t i = 0; i < SONAR_NUM; i++) {
-    Serial.print(i);
-    Serial.print("=");
-    Serial.print(cm[i]);
-    Serial.print("cm ");
-    if ((cm[0] < MX_OBS_DIS) || (cm[1] < MX_OBS_DIS)){
-      x=1;    //front
-    }
-    if ((cm[2] < MX_OBS_DIS) || (cm[3] < MX_OBS_DIS)){
-      x=2;    //left
-    }
-    if ((cm[4] < MX_OBS_DIS) || (cm[5] < MX_OBS_DIS)){
-      x=3;    //Right
-    }
-    if ((cm[6] < MX_OBS_DIS) || (cm[7] < MX_OBS_DIS)){
-      x=4;    //Back
-    }
-    
-  }
-  if ((cm[0] > MX_OBS_DIS) && (cm[1] > MX_OBS_DIS) &&(cm[2] > MX_OBS_DIS) &&(cm[3] > MX_OBS_DIS)&&(cm[4] > MX_OBS_DIS)&&(cm[5] > MX_OBS_DIS)&&(cm[6] > MX_OBS_DIS)&&(cm[7] > MX_OBS_DIS) ){
-    x=0;
-  }
-  Serial.println();
+  // Detect obstacles based on the distance
+
+  if ((cm[0] != 0 && cm[0]<obs_distance) or  (cm[1] != 0 && cm[1]<obs_distance)) obstacle_array[0] = 1;
+  else obstacle_array[0] = 0;
+
+  if ((cm[2] != 0 && cm[2]<obs_distance) or  (cm[3] != 0 && cm[3]<obs_distance)) obstacle_array[1] = 1;
+  else obstacle_array[1] = 0;
+
+  if ((cm[4] != 0 && cm[4]<obs_distance) or  (cm[5] != 0 && cm[5]<obs_distance)) obstacle_array[2] = 1;
+  else obstacle_array[2] = 0;
+
+  if ((cm[6] != 0 && cm[6]<obs_distance) or  (cm[7] != 0 && cm[7]<obs_distance)) obstacle_array[3] = 1;
+  else obstacle_array[3] = 0;
+
 }
